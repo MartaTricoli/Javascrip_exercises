4a593a95c46b1ef7019da518e47adc2c1963d5e4
Marta <martatricoli42@gmail.com>
2023-11-09 12:26:34 +0100

    create new file template_string_3.js

---
+++ template_string_3.js
@@ -0,0 +1,22 @@
+//Crea una template string che genera un messaggio di saluto con un nome e un messaggio personalizzato.
+
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = `Hello ${name}! ${message}`;
+console.log(greeting);
+
+//ho trovato questo modo per ottenere un input da terminale, è correto?
+
+let userName = "";
+const readline = require("readline"); 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+rl.question("Please, type your name here: ", answer => {
+    userName = answer;
+    console.log(`Hello ${userName}! ${message}`);
+    rl.close();
+});
