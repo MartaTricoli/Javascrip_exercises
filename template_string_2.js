1d3cc7ea6dbb0c7f15df964dbda74c377551d72c
Marta <martatricoli42@gmail.com>
2023-11-09 12:08:53 +0100

    create new file template_string_2.js

---
+++ template_string_2.js
@@ -0,0 +1,36 @@
+//Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+const breakLines = `«Forse pensate che non son bello,
+ma non giudicate da quel che vedete
+io ve lo giuro che mi scappello
+se uno più bello ne troverete.
+Potete tenervi le vostre bombette
+i vostri cilindri lucidi e alteri,
+son io quello che al posto vi mette
+e al mio confronto gli altri son zeri.
+Non c’è pensiero che nascondiate
+che il mio potere non sappia vedere,
+quindi indossatemi ed ascoltate
+qual è la casa in cui rimanere.
+forse Grifondoro la vostra via,
+culla dei coraggiosi di cuore:
+audacia, fegato, cavalleria
+fan di quel luogo uno splendore.
+O forse è a Tassorosso la vostra vita,
+dove chi alberga è giusto e leale:
+qui la pazienza regna infinita
+e il duro lavoro non è innaturale.
+Oppure Corvonero, il vecchio e il saggio,
+se siete svegli e pronti di mente,
+ragione e sapienza qui trovan linguaggio
+che si confà a simile gente.
+O forse a Serpeverde, ragazzi miei,
+voi troverete gli amici migliori
+quei tipi astuti e affatto babbei
+che qui raggiungono fini ed onori!
+Venite dunque senza paure
+E mettetemi in capo all’istante
+Con me sarete in mani sicure
+Perché io sono un Cappello Parlante!»`;
+
+console.log(breakLines);
