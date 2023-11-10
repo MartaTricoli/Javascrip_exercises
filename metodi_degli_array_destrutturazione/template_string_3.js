9d7ed025b0c2fe1c4a9f7025dbcbb0595b2a3f91
Marta <martatricoli42@gmail.com>
2023-11-10 17:05:59 +0100

    remove user input feature

--- metodi_degli_array_destrutturazione/template_string_3.js
+++ metodi_degli_array_destrutturazione/template_string_3.js
@@ -5,17 +5,3 @@ const message = "Welcome to our website!";
 const greeting = `Hello ${name}! ${message}`;
 console.log(greeting);
 
-//ho trovato questo modo per ottenere un input da terminale, è correto?
-
-let userName = "";
-const readline = require("readline"); 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-rl.question("Please, type your name here: ", answer => {
-    userName = answer;
-    console.log(`Hello ${userName}! ${message}`);
-    rl.close();
-});
