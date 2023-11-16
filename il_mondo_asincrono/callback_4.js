1dd3b93e625d0133899eb9edbe05bb2d5a7696c8
Marta <martatricoli42@gmail.com>
2023-11-16 12:42:48 +0100

    create new file callback_4.js

---
+++ il_mondo_asincrono/callback_4.js
@@ -0,0 +1,24 @@
+/*Crea una funzione chiamata runCallbacks che accetta un array di funzioni di callback come argomento.
+La funzione dovrebbe eseguire ogni callback nell'ordine in cui appaiono nell'array.*/
+
+function runCallbacks(callbacks) {
+    if (!Array.isArray(callbacks) || callbacks.filter(callback => typeof callback !== "function").length !== 0) {
+        throw new Error("callbacks must be a valid array. callbacks must contain only functions.")
+    }
+    
+    callbacks.forEach(callback => callback());
+}
+
+function firstCallback() {
+   //...
+}
+
+function secondCallback() {
+    //..
+}
+  
+function thirdCallback() {
+    //..
+}
+const callbackArray = [firstCallback, secondCallback, thirdCallback];
+runCallbacks(callbackArray);
