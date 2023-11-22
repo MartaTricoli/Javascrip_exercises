7ca72bc9569673af795c025c3766a63746aba1ef
Marta <martatricoli42@gmail.com>
2023-11-22 23:12:18 +0100

    create new file promise_2.js

---
+++ il_mondo_asincrono/promise_2.js
@@ -0,0 +1,47 @@
+/*
+1. Crea una catena di Promises per simulare un'operazione asincrona in più fasi.
+2. La prima Promise recupera i dati dell'utente { id: 1, name: 'John' }. 
+3. La seconda Promise recupera i post dell'utente ['Post 1', 'Post 2', 'Post 3'].
+4. Infine, chiama le funzioni per recuperare e stampare in console il nome dell'utente e i titoli dei post.*/
+
+//const user = [1, 2, 3]; for checking possible errors.
+//const posts = "Post 1, Post 2, Post 3"; for checking possible errors.
+
+const user = { id: 1, name: 'John' };
+const posts = ['Post 1', 'Post 2', 'Post 3'];
+
+function userData (obj) {
+    return new Promise ((resolve, reject) => {
+        if (typeof obj === "object" && !Array.isArray(obj)) {
+            let userData = {};
+            resolve (userData = obj);
+        } else {
+            reject ("obj is not a valid object");
+        }
+    });
+}
+
+function userPost (array) {
+    return new Promise ((resolve, reject) => {
+        if (Array.isArray(array)) {
+            let userPost = [];
+            resolve (userPost = array);
+        } else {
+            reject ("array must be a valid array");
+        }
+    })
+}
+
+userData(user).then(() => {
+    console.log(`user name: ${user.name}`);
+}).catch((error) => {
+    console.log(error);
+});
+
+userPost(posts).then(() => {
+    posts.forEach(post => console.log(post));
+}).catch((error) => {
+    console.log(error);
+});
+
+//richiede un setTimeout???? Boh per adesso lascio così.
