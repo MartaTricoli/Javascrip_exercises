6a0cb8cbed6b84acbcb9371cdda5681e91112335
Marta <martatricoli42@gmail.com>
2023-11-25 00:10:10 +0100

    delete new Erorr, just message of error. Not sure if it is corret

--- il_mondo_asincrono/promise_5.js
+++ il_mondo_asincrono/promise_5.js
@@ -18,7 +18,7 @@ function promise_BasedFunction (arg1, arg2) {
             if (result % 2 !== 0) {
                 resolve (result);
             } else {
-                reject (new Error("Result is not odd!"));
+                reject ("Result is not odd!");
             }
         }, 1000);
     });
