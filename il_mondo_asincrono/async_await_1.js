4f247b329b1110f7bc2e5f01c0fe05965e960be3
Marta <martatricoli42@gmail.com>
2023-11-25 00:13:56 +0100

    fix the formatting

--- il_mondo_asincrono/async_await_1.js
+++ il_mondo_asincrono/async_await_1.js
@@ -3,17 +3,17 @@ La funzione caricamentoDati simula il caricamento di dati asincroni con un ritar
 Implementa la funzioneottieniDatiutilizzando await per attendere il caricamento dei dati, quindi visualizza i dati ottenuti in console.*/
 
 function caricamentoDati() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Questi sono i dati ottenuti dalla fonte asincrona.");
-      }, 2000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Questi sono i dati ottenuti dalla fonte asincrona.");
+    }, 2000);
+  });
 }
   
 // implementa la funzione asincrona "ottieniDati"
 async function ottieniDati() {
-    const dati = await caricamentoDati();
-    console.log(dati);
+  const dati = await caricamentoDati();
+  console.log(dati);
 }
 
 ottieniDati();
