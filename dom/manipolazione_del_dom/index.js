4d1c11eab68d4799d64f5b9f46bd690fe9318f4a
Marta <martatricoli42@gmail.com>
2023-11-30 21:55:53 +0100

    create new directory dom, and all the file and directory inside

---
+++ dom/manipolazione_del_dom/index.js
@@ -0,0 +1,32 @@
+const $input = document.querySelector("#input");
+const $list = document.querySelector("#list");
+
+const state = {
+  inputTask: ""
+};
+
+$input.addEventListener("input", (event) => {
+  state.inputTask = event.target.value;
+});
+
+$input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    state.inputTask = event.target.value;
+    addProduct();
+  }
+});
+
+const addProduct = () => {
+  if (state.inputTask !== "") {
+    const newLi = document.createElement("li");
+    newLi.innerHTML = `
+    <input type="checkbox"/>
+    <label>${state.inputTask}</label>
+    `;
+    $list.appendChild(newLi);
+    $input.value = "";
+    state.inputTask = "";
+  }
+};
+
+
